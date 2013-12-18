9b109e7acf4002e8b74ec3c9ac6afb1ac617d00b
Sumardi Shukor <me@sumardi.net>
2013-12-19 00:48:19 +0800

    Fixed indentation.

--- tests/Elevencodes/InstagramLaravel/Tests/InstagramLaravelServiceProviderTest.php
+++ tests/Elevencodes/InstagramLaravel/Tests/InstagramLaravelServiceProviderTest.php
@@ -5,31 +5,31 @@ class InstagramLaravelServiceProviderTest extends InstagramTestCase
 	public function testRegisterServiceProviderWithPackageConfig()
 	{
 		$app = $this->setupApplication();
-        $instagram = $this->setupServiceProvider($app);
-        $app->register($instagram);
-        $instagram->boot();
+		$instagram = $this->setupServiceProvider($app);
+		$app->register($instagram);
+		$instagram->boot();
 
-        $instance = $app['instagram'];
-        $this->assertInstanceOf('Instagram\Auth', $instance);
+		$instance = $app['instagram'];
+		$this->assertInstanceOf('Instagram\Auth', $instance);
 	}
 
 	public function testRegisterServiceProviderWithInstagramSession()
 	{
 		$app = $this->setupApplication();
-        $instagram = $this->setupServiceProvider($app);
-        $session = $app['session'];
-        $session->set('instagram_access_token', 'foo');
-        $app->register($instagram);
-        $instagram->boot();
+		$instagram = $this->setupServiceProvider($app);
+		$session = $app['session'];
+		$session->set('instagram_access_token', 'foo');
+		$app->register($instagram);
+		$instagram->boot();
 
-        $instance = $app['instagram'];
-        $this->assertInstanceOf('Instagram\Instagram', $instance);
+		$instance = $app['instagram'];
+		$this->assertInstanceOf('Instagram\Instagram', $instance);
 	}
 
 	public function testServiceNameIsProvided()
 	{
 		$app = $this->setupApplication();
-        $provider = $this->setupServiceProvider($app);
-        $this->assertContains('instagram', $provider->provides());
+		$provider = $this->setupServiceProvider($app);
+		$this->assertContains('instagram', $provider->provides());
 	}
 }
